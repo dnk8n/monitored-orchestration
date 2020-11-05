3aee169ca71b2423bca23180edb14d46a6369830
Dean Kayton <deankayton@gmail.com>
2020-11-05 14:54:16 +0200

    Reference git maintained module

--- src/consul/main.tf
+++ src/consul/main.tf
@@ -95,8 +95,7 @@ data "template_file" "user_data_server" {
 module "consul_clients" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
-  # source = "git::git@github.com:hashicorp/terraform-aws-consul.git//modules/consul-cluster?ref=v0.0.1"
-  source = "./modules/consul-cluster"
+  source = "git::git@github.com:hashicorp/terraform-aws-consul.git//modules/consul-cluster?ref=v0.8.0"
 
   cluster_name  = "${var.cluster_name}-client"
   cluster_size  = var.num_clients
